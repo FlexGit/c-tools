9ec97152106f7a6903e60b7214d753628edd8eab
Dmitry <d1d1d1@inbox.ru>
2024-01-22 18:43:22 +0300

    pagespeed optimizing

--- resources/views/layouts/parts/js.blade.php
+++ resources/views/layouts/parts/js.blade.php
@@ -1,7 +1,7 @@
 <script src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
 <script src="{{ asset('js/jquery-migrate-3.0.1.min.js') }}"></script>
 <script src="{{ asset('js/jquery.magnific-popup.min.js') }}"></script>
-<script src="{{ asset('js/bootstrap.min.js') }}"></script>
-{{--<script src="{{ asset('js/aos.js') }}"></script>--}}
+{{--<script src="{{ asset('js/bootstrap.min.js') }}"></script>--}}
+<script src="{{ asset('js/aos.js') }}"></script>
 <script src="{{ asset('js/lozad.min.js') }}"></script>
 <script src="{{ asset('js/common.js?v=' . time()) }}"></script>
